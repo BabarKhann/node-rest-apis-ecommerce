3b8f2b3050385932cb7fa36ff04a33976aeddb18
BabarKhann <babarkhan138@gmail.com>
2023-10-26 23:52:59 +0500

    Create register route and controller , also add Joi validation for registerSchema and validate

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ import { APP_PORT } from "./config";
 
 const app = express();
 
+import routes from "./routes";
+
+app.use("/api", routes);
+
 app.listen(APP_PORT, () => console.log(`Listening on ${APP_PORT}`));
